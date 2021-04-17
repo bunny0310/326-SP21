d82c64b3367eec6c55c437b92cca25cae0c73891
Martin Ma <Martin Ma@DESKTOP>
2021-04-16 23:19:49 -0400

    PM-11 04/16/21 Implemented authentication token, added middleware, updated navbar to show user's name when logged in.

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const PORT = process.env.PORT || 5000;
 
-const body_parser = require("body-parser")
+const body_parser = require("body-parser");
 
 const router = require('./routes');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({extended: true}))
-app.use(body_parser.json())
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+
+app.set('trust proxy', 1);
+
+const sess = session({
+    secret: 'secret1234',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: 'auto' }
+});
+
+app.use(sess);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(body_parser.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use('/', router);
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,7 +6,19 @@ const loading = "<span class=\"spinner-border spinner-border-sm\"></span>       
 const validateLogin = () => {
     $("button#login-button").prepend(loading);
     const formData = formatData($("form#login-form"));
-    post('validate/login', 'dashboard', formData);
+    const failureFunction = (xhr) => {
+        const msg = JSON.parse(xhr.responseText).msg;
+        flash(msg, 2250, "error");
+    };
+
+    const successFunction = (data) => {
+        post('auth', formData, (data) => {
+            window.localStorage.setItem("PM-326-authToken", data.msg);
+            redirect(site + 'dashboard');
+        }, failureFunction);
+    };
+
+    post('validate/login', formData, successFunction, failureFunction);
     return false;
 };
 
@@ -28,9 +40,9 @@ const validateCreateProject = () => {
     };
 
     const successFunction = (data) => {
-        post('projects', 'dashboard', formData, (data) => {redirect(site + 'dashboard')}, failureFunction);
+        post('projects', formData, (data) => { redirect(site + 'dashboard') }, failureFunction);
     };
-    post('validate/create-project', 'dashboard', formData, successFunction, failureFunction);
+    post('validate/create-project', formData, successFunction, failureFunction);
     return false;
 }
 
@@ -50,7 +62,7 @@ const formatData = (data) => {
     return arr;
 };
 
-const post = (endpoint, redirectPage, formData, successFunction, failureFunction) => {
+const post = (endpoint, formData, successFunction, failureFunction) => {
     $.post(site + 'api/' + endpoint, formData, successFunction).fail(failureFunction);
 }
 
